08f8e3310b46d2cbe3faae6f4c09199728f525b8
LindsayZheng <hellozhenglingzhi@gmail.com>
2025-06-13 02:47:49 +0900

    update 02.08.检查用户名是否存在

--- admin/src/main/java/com/javaProject/shortlink/admin/service/UserService.java
+++ admin/src/main/java/com/javaProject/shortlink/admin/service/UserService.java
@@ -14,4 +14,11 @@ public interface UserService extends IService<UserDO> {
      * @return 用户返回实体
      */
     UserRespDTO getUserByUsername(String username);
+
+    /**
+     * 查询用户名是否存在
+     * @param username 用户名
+     * @return 用户名存在，true；不存在，false
+     */
+    Boolean hasUserName(String username);
 }
