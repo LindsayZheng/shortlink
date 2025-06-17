5016f3039fb10daa3471c21161761e6114ca68e5
LindsayZheng <hellozhenglingzhi@gmail.com>
2025-06-18 00:35:34 +0900

    update 4.09.短链接查询功能开发 - 增加创建时间及其序列化

--- project/src/main/java/com/javaProject/shortlink/project/dto/resp/ShortLinkPageRespDTO.java
+++ project/src/main/java/com/javaProject/shortlink/project/dto/resp/ShortLinkPageRespDTO.java
@@ -1,6 +1,7 @@
 package com.javaProject.shortlink.project.dto.resp;
 
 import com.baomidou.mybatisplus.annotation.TableField;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
 import java.util.Date;
@@ -22,6 +23,10 @@ public class ShortLinkPageRespDTO {
 
     private Integer validDateType;
 
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    private Date createTime;
+
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private Date validDate;
 
     @TableField("`describe`")
